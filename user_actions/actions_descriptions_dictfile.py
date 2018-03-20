c819a00b90e94509dbbb3c309f0d3c4eab73f439
TomaszSzyborski <tomasz.szyborski@gmail.com>
2018-03-20 13:04:21 +0100

    Initial user actions

---
+++ user_actions/actions_descriptions_dictfile.py
@@ -0,0 +1,5 @@
+actions_descriptions = {
+        "help": "Prints actions descriptions.",
+        "look around": "Check the contents of the room You're in.",
+        "exit": "exits the game"
+    }
---
+++ user_actions/user_actions.py
@@ -0,0 +1,15 @@
+from functools import partial
+
+from user_actions.actions_dictfile import actions
+
+
+def act(action=""):
+    """
+    Performs action from actions specified in actions_dictfile
+    (str) -> <function_call>
+
+    :param action: specified by string
+    :return: nothing
+    """
+    actions.get(action.lower(), partial(print, "No such action..."))()
+    return actions.get(action.lower(), None)
