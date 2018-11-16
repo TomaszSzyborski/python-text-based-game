423584af4ceb1525d8d835efedec0ab48ca82fe2
TomaszSzyborski <tomasz.szyborski@gmail.com>
2018-11-16 13:55:06 +0100

    textris

--- main/user_actions/colorize_examples.py
+++ main/user_actions/colorize_examples.py
@@ -1,10 +1,18 @@
-from colorama import Fore
+from colorama import Fore, Back
 
 
-def _Colorize(colour: Fore, text: str):
+def _Colorize(background: Back = "", foreground: Fore = "", text: str = ""):
     # return getattr(Fore, color) + text + Fore.RESET
-    return f"{colour}{text}{Fore.RESET}"
+    return f"{background}{foreground}{text}{Fore.RESET}{Back.RESET}"
 
-print(_Colorize(Fore.RED, "asdasd"))
+# print(_Colorize(Fore.RED, "asdasd"))
 # print(Fore.GREEN + "asdasd" + Fore.RESET)
-# print(_Colorize("BLUE", "lololol"))
+# print(_Colorize(Fore.BLUE, "lololol"))
+# print(_Colorize(Back.BLUE, "lololol"))
+
+
+print(_Colorize(foreground=Fore.RED, text="asdasd"))
+print(_Colorize(foreground=Fore.GREEN, text="lololol"))
+# print(Fore.GREEN + "asdasd" + Fore.RESET)
+print(_Colorize(foreground=Fore.BLUE, text="lololol"))
+print(_Colorize(background=Back.CYAN, foreground=Fore.YELLOW, text="lololol"))
